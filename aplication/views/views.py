550a228e2d6bb6658070f4d281591726b5f92782
Felipe <ld.desenvolvimentos@gmail.com>
2018-06-07 16:02:50 -0300

    links do menu para os templates funcionando | templates cadastro cliente finalizado

--- aplication/views/views.py
+++ aplication/views/views.py
@@ -3,17 +3,41 @@ from aplication import app
 
 
 # Pagina inicial do site
-@app.route('/')
-def home():
+@app.route('/login')
+def login():
     return render_template("login.html")
 
-#@app.route('/index')
-#def index():
-#    return render_template('index.html')
+@app.route('/')
+def index():
+    return render_template('index.html')
 
+@app.route('/cad-client')
+def cad_cliente():
+    return render_template('cad_cliente.html')
+
+@app.route('/list_cliente')
+def list_cliente():
+    return render_template('list_cliente.html')
+
+@app.route('/new_orca')
+def new_orca():
+    return render_template('new_orca.html')
+
+@app.route('/edit_orca')
+def edit_orca():
+    return render_template('edit_orca.html')
+
+@app.route('/list_orca')
+def list_orca():
+    return render_template('list_orca.html'), 200
+
+
+@app.route('/conf_empresa')
+def conf_empresa():
+    return render_template('conf_empresa.html')
 
 
 # caso a pagina nao seja encontrada sera redirecionada para pagina escolhida
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('404.html'), 404
+    return render_template('_404.html'), 404
